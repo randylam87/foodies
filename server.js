a47c6ebe3aec75f16ad8f3975c6fa1f15ea67b18
Randy <randylam87@gmail.com>
2017-09-14 07:00:36 -0700

    began refactoring home.js as well as css. removed bg  url and added it as a static file. removed useless code, removed console logs

--- server.js
+++ server.js
@@ -41,7 +41,6 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({
     type: 'application/vnd.api+json'
 }));
-app.use(cookieParser());
 
 // Serve files from the public folder
 app.use(express.static(path.resolve(__dirname, 'build')));
